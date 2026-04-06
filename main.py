f31e043d02ccf0f2d3bb119a623f6af797fb41fd
Manuel <manuelgarcesprofe@gmail.com>
2026-04-06 17:25:05 +0200

    Change Port

--- main.py
+++ main.py
@@ -45,7 +45,7 @@ def delete_post(post_id: str):
     raise HTTPException(status_code=404, detail="No se encontró el post para eliminar")
 
 def run_app():
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8080)
 
 if __name__ == "__main__":
     run_app()
